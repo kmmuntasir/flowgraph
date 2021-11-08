11e019db9486fc6eb3ddd81edce3c4df8da055dc
Muntasir Billah Munna <kmmuntasir@gmail.com>
2021-11-09 01:48:35 +0600

    Update default controller

--- bin/controllers/Home.php
+++ bin/controllers/Home.php
@@ -5,45 +5,32 @@ class Home extends User_Controller {
 	
 	function __construct() {
 	    parent::__construct(true);
-	    // Loading Models
-	    // $this->load->model($this->module."/m_admin");
 
 	    $this->data['page_title'] = '';
 	    $this->data['controller'] .= $this->router->fetch_class();
 	}
 
 	function index() {
-		// $data = $this->data;
-		// $data['page'] = $this->router->fetch_class();
-		// $data['page_title'] = ucfirst($this->router->fetch_class());
-		// $data['graph'] = true;
-		// $data['graph_id'] = NULL;
-		// $this->printer($data, true);
-		// $this->load->view($this->viewpath.'v_main', $data);
 
 		redirect($this->data['controller']);
 	}
-/*
-	function viewgraph($graph_id=NULL) {
-		$data = $this->data;
-		$data['page'] = $this->router->fetch_method();
-		$data['page_title'] = ucfirst($this->router->fetch_method());
-		$data['graph'] = true;
-		$data['graph_id'] = $graph_id;
-		// $this->printer($data, true);
-		$this->load->view($this->viewpath.'v_main', $data);
-	}
 
 	function default_graph_json($return=false) {
-		$json_string = '{"class": "go.GraphLinksModel","linkFromPortIdProperty": "fromPort","linkToPortIdProperty": "toPort","nodeDataArray": [],"linkDataArray": []}';
-		if($return) return $json_string;
+		$json_string = '{
+           "class":"go.GraphLinksModel",
+           "linkFromPortIdProperty":"fromPort",
+           "linkToPortIdProperty":"toPort",
+           "nodeDataArray":[],
+           "linkDataArray":[]
+        }';
+		if ($return) return $json_string;
 		else echo $json_string;
 	}
 	function fetch_graph_json($graph_id=NULL) {
 		if(!$graph_id) echo $this->default_graph_json(true);
 		else {
 			$json = $this->m_home->fetch_graph_json($graph_id);
-			if($json) echo $json->graph_json;
+			if($json) $this->printer($json->graph_json);
 			else echo "Invalid Graph ID";
 		}
 	}
@@ -60,5 +47,4 @@ class Home extends User_Controller {
 
 		}
 	}
-	*/
 }
